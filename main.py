29e7ca029d7b30f0b6350f3b86ee61810ebd3a1c
kevinpy948 <kap99876@gmail.com>
2025-03-05 12:35:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from ui.screen import S
-from src.sprite import Center
-Center=Center()
+from src.dfexception import NotSimplfiedException
 pygame.init()
+fps=60
+clock=pygame.time.Clock()
+screen=pygame.display.set_mode((185+BoundaryLength*2,205+BoundaryLength))
+pygame.display.set_caption("棋盤圖片顯示")
 a=True
 while a:
+    clock.tick(fps)
+    screen.fill((255, 255, 255))
+    S()
     mospos=pygame.mouse.get_pos()
     print(mospos)
     for events in pygame.event.get():
         if events.type==pygame.QUIT:
-            pygame.quit()
-            Center.init()
-            
-    S()
-    
+            a=False
+
 pygame.quit()
