c589cc7394ac6e3b647cdd672d88301c14b9f6a9
hactoer <arieslibra901@gmail.com>
2025-03-08 17:38:26 +0800

    Refactor screen initialization and update rendering logic in sprite and spritecontroller

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import pygame
 from ui.screen import S
-from src.UnitChanger import BoundaryLength
 from src.sprite import center
 pygame.init()
 fps=60
 clock=pygame.time.Clock()
-screen=pygame.display.set_mode((185+BoundaryLength*2,205+BoundaryLength*2))
 pygame.display.set_caption("棋盤圖片顯示")
 a=True
 center.init()
 while a:
     clock.tick(fps)
-    screen.fill((255, 255, 255))
-    S()
     mospos=pygame.mouse.get_pos()
     print(mospos)
     for events in pygame.event.get():
         if events.type==pygame.QUIT:
             a=False
-
+    S()
 pygame.quit()
--- src/sprite.py
+++ src/sprite.py
@@ -4,12 +4,28 @@ from .UnitChanger import *
 from .spritecontroller import *
 import numpy as np
 from ui.screen import screen
+Dict={
+            'rch':(rchariots,runner.r().ch),
+            'rho':(rhorses,...),
+            'rel':(relephants,...),
+            'rad':(radvisors,...),
+            'rge':(rgeneral,...),
+            'rca':(rcannos,...),
+            'rso':(rsoiders,...),
+            'bch':(bchariots,...),
+            'bho':(bhorses,...),
+            'bel':(belephants,...),
+            'bad':(badvisors,...),
+            'bge':(bgeneral,...),
+            'bca':(bcannos,...),
+            'bso':(bsoiders,...),
+    }
 class Center:
     def init(self):
         'To initialize the chessboard'
         for i in range(len(Matrix)):
             for j in range(len(Matrix[i])):
-                if Matrix[i][j]!='000' and Matrix[i][j] in Dict:
+                if Matrix[i][j] in Dict:
                     screen.blit(Dict[Matrix[i][j]][0],MTP(i,j))
     def check(self,mospos:tuple):
         for events in pygame.event.get():
@@ -21,4 +37,3 @@ class Center:
     def prerun(self):
        ...
 center=Center()
-        
