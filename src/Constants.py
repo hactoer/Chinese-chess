4940e97818581a099beeecc45f6bec8ed2e64ce6
hactoer <arieslibra901@gmail.com>
2025-03-12 00:38:18 +0800

    Change

---
+++ src/Constants.py
@@ -0,0 +1,7 @@
+'''All needed Constants'''
+BoundaryLength=20
+OriginalBoardSize=(185,205)
+BlockLength=(OriginalBoardSize[0]*2-BoundaryLength)//8
+r=(BlockLength,BlockLength)
+sigma_yi_=12
+sigma_xj_=26
--- ui/screen.py
+++ ui/screen.py
@@ -1,7 +1,7 @@
 import pygame
 from asserts.images import images
 from INIT import version
-from src.UnitChanger import BoundaryLength,OriginalBoardSize
+from src.Constants import *
 screen=pygame.display.set_mode((OriginalBoardSize[0]*2+BoundaryLength*2,OriginalBoardSize[1]*2+BoundaryLength))
 pygame.display.set_caption(f'Chinese Chess {version}')
 def S():
